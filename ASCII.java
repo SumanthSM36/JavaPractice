85ef8c6aea3110e2754f2a820dfe725929c55127
Sumanth SM <56683069+SumanthSM36@users.noreply.github.com>
2020-09-18 18:08:15 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ascii {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a Character:");
+		char ch=scn.next().charAt(0);
+		int ascii=ch;
+		System.out.println("The ASCII value of "+ ch + " is: " + ascii);
+	}
+}
